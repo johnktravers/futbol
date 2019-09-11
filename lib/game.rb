7a882ac5182aee2fd79ee6560e092ca3b09306e5
John Travers <johnk.travers@gmail.com>
2019-09-11 12:47:36 -0600

    Remove unnecessary data from game and game_team classes and tests
    Co-authored-by: Ryan Hantak <47759923+rhantak@users.noreply.github.com>
    Co-authored-by: David Barriga-Gomez <50917634+DavidBarriga-Gomez@users.noreply.github.com>
    Co-authored-by: Carleigh Crockett <47065120+Capleugh@users.noreply.github.com>

--- lib/game.rb
+++ lib/game.rb
@@ -2,25 +2,19 @@ class Game
   attr_reader :game_id,
               :season,
               :type,
-              :date_time,
               :away_team_id,
               :home_team_id,
               :away_goals,
-              :home_goals,
-              :venue,
-              :venue_link
+              :home_goals
 
   def initialize(game_info)
     @game_id = game_info[0]
     @season = game_info[1]
     @type = game_info[2]
-    @date_time = game_info[3]
     @away_team_id = game_info[4]
     @home_team_id = game_info[5]
     @away_goals = game_info[6].to_i
     @home_goals = game_info[7].to_i
-    @venue = game_info[8]
-    @venue_link = game_info[9]
   end
 
 end
